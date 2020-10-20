fe3c5ccb93e139206899f27b379b89e45d9a7bbf
Gabriel Eduardo Acosta Nuñez <gacosta@confisa.do>
2020-10-20 15:10:40 -0400

    appCenter include!

--- XFDelivery/XFDelivery/App.xaml.cs
+++ XFDelivery/XFDelivery/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using Xamarin.Forms;
+﻿using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using Xamarin.Forms;
 using XFDelivery.Interfaces;
 using XFDelivery.Views;
 
@@ -10,16 +13,20 @@ namespace XFDelivery
         {
             InitializeComponent();
 
-            Device.SetFlags(new[] { "Shapes_Experimental" });
+            //Device.SetFlags(new[] { "Shapes_Experimental" });
 
             MainPage = new NavigationPage(new MainPage());
 
-            if (Device.RuntimePlatform == Device.Android)
-                DependencyService.Get<IStatusBarStyle>().ChangeTextColor();
+            //if (Device.RuntimePlatform == Device.Android)
+            //    DependencyService.Get<IStatusBarStyle>().ChangeTextColor();
         }
 
         protected override void OnStart()
         {
+            AppCenter.Start("ios=1cd4c6d9-5ab8-4e5a-b0d6-51d22bad4897;" +
+                  "uwp={Your UWP App secret here};" +
+                  "android={Your Android App secret here}",
+                  typeof(Analytics), typeof(Crashes));
         }
 
         protected override void OnSleep()
